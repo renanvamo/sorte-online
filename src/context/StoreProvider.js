3f904de5a68ffa2d74a749b97dd02a0abf4d91cb
Renan <renan.vamo@gmail.com>
2022-02-12 17:08:02 -0300

    feat: cria funçoes para adicionar e remover o produto do carrinho

--- src/context/StoreProvider.js
+++ src/context/StoreProvider.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StoreContext from './StoreContext';
 
 function StoreProvider({ children }) {
-  const args = {};
+  const [shoppingCart, setShoppingCart] = useState([]);
+
+  const addToCart = (cart) => {
+    setShoppingCart([
+      ...shoppingCart,
+      cart
+    ]);
+  };
+
+  const removeFromCart = (id) => {
+    const filteredProducts = shoppingCart.filter((item) => (
+      item.id !== id
+    ));
+    setShoppingCart(filteredProducts);
+  };
+
+  const args = {
+    shoppingCart,
+    setShoppingCart,
+    addToCart,
+    removeFromCart
+  };
 
   return (
     <StoreContext.Provider value={ args }>
