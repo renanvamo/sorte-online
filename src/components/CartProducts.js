0b4635ac38ad8c59f62f53067e8347951bfcbff6
Renan <renan.vamo@gmail.com>
2022-02-12 19:11:53 -0300

    feat: cria componente que renderiza a tabela de itens do shoppingCart

--- src/components/CartProducts.js
+++ src/components/CartProducts.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import StoreContext from '../context/StoreContext';
+import './styles/cartProduct.css';
 
 export default function CartProducts() {
+  const { shoppingCart } = useContext(StoreContext);
+
   return (
-    <div>CartProducts</div>
+    <section className='table-container'>
+      <table>
+        <thead>
+          <tr>
+            <th>Product</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        { shoppingCart.map((item) => (
+          <tbody>
+            <tr>
+              <td>
+                <div>
+                  <img src={ item.image } alt={ `imagem do produto ${item.title}` }/>
+                  <span>{ item.title }</span>
+                </div>
+              </td>
+              <td>1</td>
+              <td>{ item.price }</td>
+            </tr>
+          </tbody>
+        )) }
+      </table>
+      <span className='total-price'>TOTAL $10.00</span>
+    </section>
   );
 }
