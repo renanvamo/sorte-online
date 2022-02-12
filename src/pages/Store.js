e8ee316a22335163c1dd265ba63e4b8fc1e9d898
Renan <renan.vamo@gmail.com>
2022-02-12 14:32:43 -0300

    feat: chama o componente Products dentro do componente Store

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Products from '../components/Products';
 
 export default function Store() {
   return (
     <div>
       <Header />
+      <Products />
     </div>
   );
 }
