0e4276f4ef4a2a339d1fa6fb64cdba4efc8faf61
Renan <renan.vamo@gmail.com>
2022-02-12 13:13:32 -0300

    feat: adiciona navegaçao nos botoes do header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './styles/header.css';
 
 export default function Header() {
+  const navigate = useNavigate()
+
+  const navigateTo = (path) => {
+    navigate(path);
+  } 
   return (
     <header className='header'>
       <div className='nav-left'>
-        <div className='nav-element'>
+        <div className='nav-element' onClick={ () => navigateTo('/store') }>
           Home
         </div>
         <div className='nav-element'>
@@ -13,7 +19,7 @@ export default function Header() {
         </div>
       </div>
       <div className='nav-rigth'>
-        <div className='nav-element'>
+        <div className='nav-element' onClick={ () => navigateTo('/shopping-cart') }>
           Cart
         </div>
       </div>
