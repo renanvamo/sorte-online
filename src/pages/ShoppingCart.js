3062732b8f26fc0b479759192ad7cc8b068d1a14
Renan <renan.vamo@gmail.com>
2022-02-12 17:25:14 -0300

    feat: cria novo componente para renderizar os produtos do carrinho e chama no ShoppingCart

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import CartProducts from '../components/CartProducts.js';
 
 export default function ShoppingCart() {
   return (
     <div>
       <Header />
+      <CartProducts />
     </div>
   );
 }
