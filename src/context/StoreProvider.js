f5bd2b4cfe313313d5f9ce51761c28ffb2b2f45a
Renan <renan.vamo@gmail.com>
2022-02-12 17:20:45 -0300

    refactor: refatora funcoes para simplicar leitura

--- src/context/StoreProvider.js
+++ src/context/StoreProvider.js
@@ -4,19 +4,9 @@ import StoreContext from './StoreContext';
 function StoreProvider({ children }) {
   const [shoppingCart, setShoppingCart] = useState([]);
 
-  const addToCart = (cart) => {
-    setShoppingCart([
-      ...shoppingCart,
-      cart
-    ]);
-  };
+  const addToCart = (cart) => setShoppingCart([ ...shoppingCart, cart ]);
 
-  const removeFromCart = (id) => {
-    const filteredProducts = shoppingCart.filter((item) => (
-      item.id !== id
-    ));
-    setShoppingCart(filteredProducts);
-  };
+  const removeFromCart = (id) => setShoppingCart(shoppingCart.filter((item) => item.id !== id ));
 
   const args = {
     shoppingCart,
