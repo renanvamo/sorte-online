fd95dbea0db6310d30d8e5839045f490f8118ec5
Renan <renan.vamo@gmail.com>
2022-02-12 20:06:53 -0300

    feat: cria funcao que soma o valor do shoppingCart dentro do context

--- src/context/StoreProvider.js
+++ src/context/StoreProvider.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import StoreContext from './StoreContext';
 
 function StoreProvider({ children }) {
   const [shoppingCart, setShoppingCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   const addToCart = (cart) => setShoppingCart([ ...shoppingCart, cart ]);
 
   const removeFromCart = (id) => setShoppingCart(shoppingCart.filter((item) => item.id !== id ));
 
+  const sumCartValue = () => setTotalPrice(shoppingCart.reduce((acc, { price }) => acc + price, 0));
+  useEffect(sumCartValue, [shoppingCart]);
+
   const args = {
     shoppingCart,
     setShoppingCart,
     addToCart,
-    removeFromCart
+    removeFromCart,
+    totalPrice
   };
 
   return (
